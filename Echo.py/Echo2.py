d6dce4c99e8fda2fb5a8aa1a1dc5fb00f27bc6e6
elemental9 <43331169+elemental9@users.noreply.github.com>
2018-11-21 08:19:00 +1300

    Update Echo2.py

--- Echo.py/Echo2.py
+++ Echo.py/Echo2.py
@@ -1,4 +1,8 @@
 #paragraph space
-def echospace(paragraphspaces=1):
+def Echospace(paragraphspaces=1):
 	for i in range(paragraphspaces):
 		print("", end="/n")
+
+#print letters/numbers
+def Echoprint(message):
+	print(str(message))
