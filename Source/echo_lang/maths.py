8c6a496d696ae4bdcb1148f9a44667f11c753613
elemental9 <43331169+elemental9@users.noreply.github.com>
2018-11-22 08:29:53 +1300

    Create maths.py

---
+++ Source/echo_lang/maths.py
@@ -0,0 +1,50 @@
+#sin of a number
+def shinysin(number):
+	import math
+	return math.sin(number)
+  
+  #tan of a number
+def shinytan(number):
+	import math
+	return math.tan(number)
+  
+#hypot of a number
+def shinyhypot(number):
+	import math
+	return math.hypot(number)
+  
+#atan of a number
+def shinyatan(number):
+	import math
+	return math.atan(number)
+
+#asin of a number
+def shinyasin(number):
+	import math
+	return math.asin(number)
+
+#get value of pi
+def shinepi():
+	import math
+	return math.pi
+
+#get value of e
+def shinye():
+	import math
+	return math.e
+  
+#divide 2 numbers
+def shinedivide(firstnumber, secondnumber):
+	return firstnumber / secondnumber
+
+#multiply 2 numbers
+def shinetimes(firstnumber, secondnumber):
+	return firstnumber * secondnumber
+
+#add 2 numbers
+def shineadd(firstnumber, secondnumber):
+	return firstnumber + secondnumber
+
+#minus 2 numbers
+def shineminus(firstnumber, secondnumber):
+	return firstnumber - secondnumber
