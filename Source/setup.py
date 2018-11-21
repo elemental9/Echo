59c1455573587b0161fa03df42fcee6ac78faed6
Richie Bendall <richiebendall@gmail.com>
2018-11-21 16:36:18 +1300

    Update setup.py

--- Source/setup.py
+++ Source/setup.py
@@ -6,7 +6,7 @@ with open("../README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name="Echo",
+    name="echo-lang",
     version="0.0.1",
     author="elemental9",
     description=
