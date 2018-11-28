2f9b3d16bf1d32a543e22f8cbf1f1d7a5bebaebb
Richie Bendall <richiebendall@gmail.com>
2018-11-28 19:47:07 +1300

    Update setup.py

--- Source/setup.py
+++ Source/setup.py
@@ -1,25 +1,5 @@
 # Import setuptools module
 import setuptools
 
-# Parse the readme markdown file
-with open("../README.md", "r") as fh:
-    LONG_DESCRIPTION = fh.read()
-
-setuptools.setup(
-    name="echo-lang",
-    author="elemental9",
-    description="A code os.",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/elemental9/Echo",
-    packages=setuptools.find_packages(),
-    classifiers=(
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ),
-    pbr=True)
+# Setup module
+setuptools.setup(setup_requires=['pbr'], pbr=True)
