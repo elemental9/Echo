4457f58bc0acc933e07e705d4b932df4a0cac2e5
Richie Bendall <richiebendall@gmail.com>
2018-12-02 20:22:29 +1300

    Update setup.py

--- Source/setup.py
+++ Source/setup.py
@@ -10,5 +10,5 @@ setuptools.setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     version_format='{tag}',
-    setup_requires=['setuptools-git-version'],
+    setup_requires=['setuptools-git-version']
 )
