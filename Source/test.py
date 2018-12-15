c6a94a108acf1b873063ac081dbd9594b146d5ca
Richie Bendall <richiebendall@gmail.com>
2018-12-15 18:10:51 +1300

    Update code testing

--- Source/test.py
+++ Source/test.py
@@ -1,4 +1,15 @@
-import echo_lang as echo
-echo.Echo("Hello World!")
-echo.Echopi()
-echo.Echo("Free!", 20)
+import echo_lang as e
+import unittest
+
+
+class TestMethods(unittest.TestCase):
+    def test_Echo(s):
+        e.Echo("Hello World!")
+        e.Echo("Free!", 20)
+
+    def test_Echopi(s):
+        e.Echopi()
+
+
+if __name__ == '__main__':
+    unittest.main()
